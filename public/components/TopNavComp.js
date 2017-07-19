7518f7a469f67f10f7c83776a4b6710b5cbd7f16
Gregory Testo <cryptomonger@hotmail.com>
2017-07-19 00:22:59 -0700

    refactored topNav JSX

--- public/components/TopNavComp.js
+++ public/components/TopNavComp.js
@@ -1,70 +1,72 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { render } from 'react-dom';
 
-export const TopNavComp = () => (
-  <div>
-    <div className="topAccordion accordion">
-      <ul>
-        <li>
-          <div>
-            <Link to="/">
-              <h2>gregorytesto.com</h2>
-              <p>This is an accumulation of my work. Take a look around.</p>
-            </Link>
-          </div>
-        </li>
-        <li>
-          <div>
-            <Link to="/emics">
-                <h2>EMICS</h2>
-                <p>Emergency Mobile Information & Communication System: A portable long range IOT device.</p>
-              </Link>
-          </div>
-        </li>
-        <li>
-          <div>
-            <Link to="/testophotography">
-              <h2>Testo Photography</h2>
-              <p>This is my personal photography website. I hope you enjoy your visit.</p>
-            </Link>
-          </div>
-        </li>
-        <li>
-          <div>
-            <Link to="/prints">
-              <h2>Canvas Prints</h2>
-              <p>An ecommerce website selling canvas prints. Window shoppers not just accepted but encouraged.</p>
-            </Link>
-          </div>
-        </li>
-        <li>
-          <div>
-            <Link to="/sangria">
-              <h2>Sangria NYC</h2>
-              <p>A New York City community sangria recipe database.</p>
-            </Link>
-          </div>
-        </li>
-        <li>
-          <div>
-            <Link to="/quickstarter">
-              <h2>Quick Starter</h2>
-              <p>This is my crowdfunding page for my canvas prints, photo book and my EMICS project.</p>
-            </Link>
-          </div>
-        </li>
-        <li>
-          <div>
-            <Link to="/photobot">
-              <h2>Photo Bot</h2>
-              <p>2nd Edition Ver. 2.31</p>
-              <p>A voice activated photography web app that provides photo tips and locations.</p>
-            </Link>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
 
+export class TopNavComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      websiteArr: [
+        {
+          name: "gregorytesto.com",
+          description: "This is an accumulation of my work. Take a look around.",
+          link: "/"
+        },
+        {
+          name: "EMICS",
+          description: "Emergency Mobile Information & Communication System: A portable long range IOT device.",
+          link: "/emics"
+        },
+        {
+          name: "Testo Photography",
+          description: "This is my personal photography website. I hope you enjoy your visit.",
+          link: "/testophotography"
+        },
+        {
+          name: "Canvas Prints",
+          description: "An ecommerce website selling canvas prints. Window shoppers not just accepted but encouraged.",
+          link: "/prints"
+        },
+        {
+          name: "Sangria NYC",
+          description: "A New York City community sangria recipe database.",
+          link: "/sangria"
+        },
+        {
+          name: "Quick Starter",
+          description: "This is my crowdfunding page for my canvas prints, photo book and my EMICS project.",
+          link: "/quickstarter"
+        },
+        {
+          name: "Photo Bot: 2nd Edition Ver. 2.31",
+          description: "A voice activated photography web app that provides photo tips and locations.",
+          link: "/photobot"
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div className="topAccordion accordion">
+          <ul>
+            {this.state.websiteArr.map(function(array){
+              return(
+                <li key={array.name}>
+                  <div>
+                    <Link to={array.link}>
+                      <h2>{array.name}</h2>
+                      <p>{array.description}</p>
+                    </Link>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
 export default TopNavComp;
