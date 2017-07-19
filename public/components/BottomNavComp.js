f9bc90741cf47798a2ddecc2038be30e3793e935
Gregory Testo <cryptomonger@hotmail.com>
2017-07-19 00:52:29 -0700

    refactored bottomNav code

--- public/components/BottomNavComp.js
+++ public/components/BottomNavComp.js
@@ -2,29 +2,70 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-export const BottomNavComp = () => (
-  <div>
-    <div className="bottomAccordion accordion">
-      <ul>
-        <li>
-          <div>
-            <Link to="/biopage">
-              <h2>About Me</h2>
-              <p>My name is Gregory Testo and here is some interesting things about me.</p>
-            </Link>
-          </div>
-        </li>
-        <li>
-          <div>
-            <Link to="/contact">
-              <h2>Contact Me</h2>
-              <p>The best way to contact me is...</p>
-            </Link>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
+export class BottomNavComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      websiteArr: [
+        {
+          name: "About Me",
+          description: "My name is Gregory Testo and here is some interesting things about me.",
+          link: "/biopage"
+        },
+        {
+          name: "Contact Me",
+          description: "The best way to contact me is...",
+          link: "/contact"
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      // <div>
+      //   <div className="bottomAccordion accordion">
+      //     <ul>
+      //       {this.state.websiteArr.map(function(array){
+      //         return(
+      //           <li key={array.name}>
+      //             <div>
+      //               <Link to={array.link}>
+      //                 <h2>{array.name}</h2>
+      //                 <p>{array.description}</p>
+      //               </Link>
+      //             </div>
+      //           </li>
+      //         );
+      //       })};
+      //     </ul>
+      //   </div>
+      // </div>
+
+
+      <div>
+        <div className="bottomAccordion accordion">
+          <ul>
+            <li>
+              <div>
+                <Link to={this.state.websiteArr[0].link}>
+                  <h2>{this.state.websiteArr[0].name}</h2>
+                  <p>{this.state.websiteArr[0].description}</p>
+                </Link>
+              </div>
+            </li>
+            <li>
+              <div>
+                <Link to={this.state.websiteArr[1].link}>
+                  <h2>{this.state.websiteArr[1].name}</h2>
+                  <p>{this.state.websiteArr[1].description}</p>
+                </Link>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+};
 
 export default BottomNavComp;
