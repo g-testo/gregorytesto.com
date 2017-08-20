4b84357df06542fee36c361057ae43d0590218b6
Gregory Testo <cryptomonger@hotmail.com>
2017-08-20 00:02:52 -0700

    adjusted number of visible pages to match what is currently completed

--- public/components/App.js
+++ public/components/App.js
@@ -27,21 +27,6 @@ export class App extends React.Component {
             {name:"Babel", icon:"devicon-babel-plain colored", url:"https://babeljs.io"},
             {name:"GitHub API", icon: "devicon-github-plain", url:"https://github.com"}
           ]
-      },
-        {
-          path: "/emics",
-          name: "EMICS",
-          gitRepoName: "EMICS-Website",
-          websiteLink: "www.emi-cs.com",
-          description: "Emergency Mobile Information & Communication System: A portable long range IOT device.",
-          webTechUsed: [
-            {name:"Node", icon:"devicon-angularjs-plain colored", url:"https://nodejs.org"},
-            {name:"Express", icon:"devicon-express-original colored", url:"https://expressjs.com"},
-            {name:"React", icon:"devicon-react-original", url:"https://facebook.github.io/react"},
-            {name:"Webpack", icon:"devicon-webpack-plain colored", url:"https://webpack.github.io"},
-            {name:"Babel", icon:"devicon-babel-plain colored", url:"https://babeljs.io"},
-            {name:"Angular", icon:"devicon-angularjs-plain", url:"https://angularjs.org"}
-          ]
         },
         {
           path: "/testophotography",
@@ -58,16 +43,6 @@ export class App extends React.Component {
           ]
         },
         {
-          path: "/prints",
-          name: "Canvas Prints",
-          gitRepoName: "canvasStore",
-          websiteLink: "www.testophotography.com/prints",
-          description: "An ecommerce website selling canvas prints. Window shoppers not just accepted but encouraged.",
-          webTechUsed: [
-            {name:"Rails", icon:"devicon-rails-plain colored", url:"http://rubyonrails.org"}
-          ]
-        },
-        {
           path: "/sangria",
           name: "Sangria NYC",
           gitRepoName: "SangriaNYC",
@@ -77,32 +52,57 @@ export class App extends React.Component {
             {name:"Rails", icon:"devicon-rails-plain colored", url:"http://rubyonrails.org"}
           ]
         },
-        {
-          path: "/quickstarter",
-          name: "Quick Starter",
-          gitRepoName: "Quickstarter",
-          websiteLink: "www.gregorytesto.com/quickstarter",
-          description: "This is my crowdfunding page for my canvas prints, photo book and my EMICS project.",
-          webTechUsed: [
-            {name:"React", icon:"devicon-react-original", url:"https://facebook.github.io/react"},
-            {name:"Webpack", icon:"devicon-webpack-plain colored", url:"https://webpack.github.io"},
-            {name:"Babel", icon:"devicon-babel-plain colored", url:"https://babeljs.io"},
-            {name:"Rails", icon:"devicon-rails-plain colored", url:"http://rubyonrails.org"}
-          ]
-        },
-        {
-          path: "/photobot",
-          name: "Photo Bot: 2nd Edition Ver. 2.31",
-          gitRepoName: "PhotoBot",
-          websiteLink: "www.gregorytesto.com/photobot",
-          description: "A voice activated photography web app that provides photo tips and locations.",
-          webTechUsed: [
-            {name:"Rails", icon:"devicon-rails-plain colored", url:"http://rubyonrails.org"},
-            {name:"Web Audio API", icon:"", url:"https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API"},
-            {name:"Google Maps API", icon:"", url:"https://www.google.com/maps/"},
-            {name:"ResponsiveVoice.JS", icon:"", url:"https://responsivevoice.org/"}
-          ]
-        }
+        // {
+        //   path: "/emics",
+        //   name: "EMICS",
+        //   gitRepoName: "EMICS-Website",
+        //   websiteLink: "www.emi-cs.com",
+        //   description: "Emergency Mobile Information & Communication System: A portable long range IOT device.",
+        //   webTechUsed: [
+        //     {name:"Node", icon:"devicon-angularjs-plain colored", url:"https://nodejs.org"},
+        //     {name:"Express", icon:"devicon-express-original colored", url:"https://expressjs.com"},
+        //     {name:"React", icon:"devicon-react-original", url:"https://facebook.github.io/react"},
+        //     {name:"Webpack", icon:"devicon-webpack-plain colored", url:"https://webpack.github.io"},
+        //     {name:"Babel", icon:"devicon-babel-plain colored", url:"https://babeljs.io"},
+        //     {name:"Angular", icon:"devicon-angularjs-plain", url:"https://angularjs.org"}
+        //   ]
+        // },
+        // {
+        //   path: "/prints",
+        //   name: "Canvas Prints",
+        //   gitRepoName: "canvasStore",
+        //   websiteLink: "www.testophotography.com/prints",
+        //   description: "An ecommerce website selling canvas prints. Window shoppers not just accepted but encouraged.",
+        //   webTechUsed: [
+        //     {name:"Rails", icon:"devicon-rails-plain colored", url:"http://rubyonrails.org"}
+        //   ]
+        // },
+        // {
+        //   path: "/quickstarter",
+        //   name: "Quick Starter",
+        //   gitRepoName: "Quickstarter",
+        //   websiteLink: "www.gregorytesto.com/quickstarter",
+        //   description: "This is my crowdfunding page for my canvas prints, photo book and my EMICS project.",
+        //   webTechUsed: [
+        //     {name:"React", icon:"devicon-react-original", url:"https://facebook.github.io/react"},
+        //     {name:"Webpack", icon:"devicon-webpack-plain colored", url:"https://webpack.github.io"},
+        //     {name:"Babel", icon:"devicon-babel-plain colored", url:"https://babeljs.io"},
+        //     {name:"Rails", icon:"devicon-rails-plain colored", url:"http://rubyonrails.org"}
+        //   ]
+        // },
+        // {
+        //   path: "/photobot",
+        //   name: "Photo Bot: 2nd Edition Ver. 2.31",
+        //   gitRepoName: "PhotoBot",
+        //   websiteLink: "www.gregorytesto.com/photobot",
+        //   description: "A voice activated photography web app that provides photo tips and locations.",
+        //   webTechUsed: [
+        //     {name:"Rails", icon:"devicon-rails-plain colored", url:"http://rubyonrails.org"},
+        //     {name:"Web Audio API", icon:"", url:"https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API"},
+        //     {name:"Google Maps API", icon:"", url:"https://www.google.com/maps/"},
+        //     {name:"ResponsiveVoice.JS", icon:"", url:"https://responsivevoice.org/"}
+        //   ]
+        // }
       ]
     };
   };
